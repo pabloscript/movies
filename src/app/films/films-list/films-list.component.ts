04990afc1643177016c03009003ada9d22c6bfa4
pabloscript <pe.kozicki@gmail.com>
2019-02-24 11:07:32 +0100

    Add film-list component

---
+++ src/app/films/films-list/films-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-films-list',
+  templateUrl: './films-list.component.html',
+  styleUrls: ['./films-list.component.scss']
+})
+export class FilmsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
