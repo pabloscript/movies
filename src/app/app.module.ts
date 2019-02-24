c15b63f2e0b6a1037da9cafcfab96b326c3a990d
pabloscript <pe.kozicki@gmail.com>
2019-02-24 12:10:32 +0100

    Update film-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FilmsListComponent } from './films/films-list/films-list.component';
+import { FilmDetailsComponent } from './films/films-list/film-details/film-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FilmsListComponent
+    FilmsListComponent,
+    FilmDetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/films/films-list/films-list.component.ts
+++ src/app/films/films-list/films-list.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import {Film} from '../film';
+import * as data from 'db.json';
 
 @Component({
   selector: 'app-films-list',
   templateUrl: './films-list.component.html',
   styleUrls: ['./films-list.component.scss']
 })
-export class FilmsListComponent implements OnInit {
 
-  constructor() { }
+export class FilmsListComponent implements OnInit {
+  public name = 'Films list';
+  public films: Film[];
+  public filmsDetailIsVisible = false;
+
+  constructor() {
+    this.films =  data.default.movies;
+  }
 
   ngOnInit() {
   }
 
+
+  public toggleFilmDetails(): void {
+    this.filmsDetailIsVisible = !this.filmsDetailIsVisible;
+  }
 }
